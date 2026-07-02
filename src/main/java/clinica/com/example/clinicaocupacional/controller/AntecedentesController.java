47aa868b3308a36e813c65602d47e17d1471b03c
Edward23-ux <edwardcampos46@gmail.com>
2026-07-02 17:03:16 -0500

    Se mejoró controller

--- src/main/java/clinica/com/example/clinicaocupacional/controller/AntecedentesController.java
+++ src/main/java/clinica/com/example/clinicaocupacional/controller/AntecedentesController.java
@@ -18,6 +18,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/antecedentes")
+@RequiredArgsConstructor
+
 public class AntecedentesController {
 
     private final AntecedentesService antecedentesService;
